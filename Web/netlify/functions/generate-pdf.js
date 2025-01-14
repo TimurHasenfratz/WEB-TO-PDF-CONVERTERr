dd099b2c575bbfec20b8410102a4557dbc9599a0
Timur <timurhasenfratz0106@gmail.com>
2025-01-14 17:08:51 +0100

    test

--- Web/netlify/functions/generate-pdf.js
+++ Web/netlify/functions/generate-pdf.js
@@ -1,10 +1,12 @@
+
+
 // netlify/functions/generate-pdf.js
 exports.handler = async function(event, context) {
     const { url, css, js } = JSON.parse(event.body);
     
     // Implementiere hier deine Logik zur PDF-Generierung
     // Zum Beispiel eine Bibliothek wie Puppeteer oder ähnliche verwenden, um die PDF zu erstellen
-    const pdfPath = 'https://example.com/output.pdf'; // Dies ist nur ein Platzhalter
+    const pdfPath = 'https://website-to-pdf-converter.netlify.app/example.pdf'; // Dies ist nur ein Platzhalter
   
     return {
       statusCode: 200,
--- Web/netlify/functions/generate-preview.js
+++ Web/netlify/functions/generate-preview.js
@@ -4,7 +4,7 @@ exports.handler = async function(event, context) {
     
     // Hier implementierst du die Logik zur Generierung der Vorschau
     // Zum Beispiel ein Bild oder HTML-Rendering (dies ist ein Platzhalter)
-    const previewImage = 'https://example.com/preview.png';
+    const previewImage = 'https://website-to-pdf-converter.netlify.app/example-1.png';
   
     return {
       statusCode: 200,
