3c50049ec53c6ca03649b505b3fa8073f4ed976f
Timur <timurhasenfratz0106@gmail.com>
2025-01-14 17:25:07 +0100

    test

--- Web/netlify/functions/generate-pdf.js
+++ Web/netlify/functions/generate-pdf.js
@@ -80,18 +80,4 @@ exports.handler = async function(event, context) {
   }
 };
 
-
-// netlify/functions/generate-pdf.js
-exports.handler = async function(event, context) {
-    const { url, css, js } = JSON.parse(event.body);
-    
-    // Implementiere hier deine Logik zur PDF-Generierung
-    // Zum Beispiel eine Bibliothek wie Puppeteer oder ähnliche verwenden, um die PDF zu erstellen
-    const pdfPath = 'https://website-to-pdf-converter.netlify.app/example.pdf'; // Dies ist nur ein Platzhalter
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ pdfPath })
-    };
-  };
   
